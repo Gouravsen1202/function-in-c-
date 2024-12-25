384511a96fa337383fef9999fb3fdc9c533131f4
Gouravsen1202 <gouravsen757@gmail.com>
2024-12-25 19:47:30 +0530

    first commit

---
+++ Untitled1.cpp
@@ -0,0 +1,52 @@
+// int  main calculation krta hia function  return jb lgana hia tb calculation krna ho function me..int main calcultion ke liye
+// void main displqy krta hia non return dixplay krrqne ke liye like print krrane ke liye as a viod main function..void main print hoga
+#include<iostream>
+using namespace std;
+
+//int main for calculation
+//int sum()
+//{
+//	int a=10;
+//	int b=20;
+//	return a+b;
+//}
+//int main(){
+//	
+//cout<<sum();
+//}
+
+
+//for printing
+//void sum()
+//{
+//	cout<<"hii";
+//}
+//int main(){
+//	sum();
+//}
+
+//
+////function with perameter;
+//int sum(int a,int b)
+//{
+//	return a+b;
+// }
+//int main(){
+//	cout<<sum(10,20);
+
+//}
+
+//function with perameter,,,,,
+//int sum(int a,int b)
+//{
+//	int c=a;
+//	int d=b;
+//	return c+d;
+//}
+//int main(){
+//	int a,b;
+//	cin>>a>>b;
+//	cout<<sum(a,b);
+//}
+
+
---
+++ function.cpp
@@ -0,0 +1,36 @@
+#include<iostream>
+using namespace std;
+//int power(){
+
+//function in sum
+
+//	int a=5;
+//	int b=20;
+//	int c=a+b;
+//	return a+b;
+//}
+//int main(){
+//	cout<<power();
+//}
+
+//function using void
+
+//void sum(){
+//	cout<<" hi btol";
+//}
+//int main(){
+//	sum();
+//	sum();
+//	sum();
+//	sum();
+//}
+
+int sum(int a, int b){
+	return a+b;
+
+}
+int main(){
+	int a, b;
+	cout<<sum(10,11);
+	cout<<sum(20,20);
+}
